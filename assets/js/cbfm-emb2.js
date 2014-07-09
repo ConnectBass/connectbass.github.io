18431c6050f73744202e8eaa91f831094d8f276d
Daigoro Izumi <izumi@d-worx.co.jp>
2014-07-09 21:18:39 +0900

    Create cbfm-emb2.js

---
+++ assets/js/cbfm-emb2.js
@@ -0,0 +1,1 @@
+var cbfm = new connectBassContactFormLoader({divId:'connectBassCampaignContactForm', oxc: 'hzC/hAg', width:800, height:850, formType:'basic', apiKey:'hzC.5ff5bf2eee0e490b', secret:'669abecb.570b24031b984509'});
