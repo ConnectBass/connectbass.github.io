a6500d0ecc944674161536a1cb888f12c0b0102f
Daigoro Izumi <izumi@d-worx.co.jp>
2014-07-10 01:25:25 +0900

    Update cbfm-emb.js

--- assets/js/cbfm-emb.js
+++ assets/js/cbfm-emb.js
@@ -1,1 +1,1 @@
-var cbfm = new connectBassContactFormLoader({divId:'connectBassCampaignContactForm', oxc: 'hzC/hAe', width:800, height:850, formType:'basic', apiKey:'hzC.5ff5bf2eee0e490b', secret:'669abecb.570b24031b984509'});
+var cbfm = new connectBassContactFormLoader({divId:'connectBassCampaignContactForm', oxc: 'hzC/hAe', width:700, height:850, formType:'basic', apiKey:'hzC.5ff5bf2eee0e490b', secret:'669abecb.570b24031b984509'});
